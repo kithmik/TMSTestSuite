4b76ed208cf8827462186b00737da481256cda3b
chinthana_senanayake <reach.senanayake@gmail.com>
2021-06-03 12:23:47 +0530

    Signup signin completed with parameterization

--- src/main/java/com/tms/pages/user/userprofile/userprofile.java
+++ src/main/java/com/tms/pages/user/userprofile/userprofile.java
@@ -1,4 +1,85 @@
 package com.tms.pages.user.userprofile;
 
+import com.tms.util.common.CommonSteps;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class userprofile {
+    private WebDriver driver;
+    private CommonSteps commonStepsObj;
+
+
+    public userprofile(WebDriver driver, CommonSteps commonStepsObj) {
+        this.driver = driver;
+        this.commonStepsObj = commonStepsObj;
+
+    }
+
+
+    public void clickOnDetailsButton(String admin,String adminPassword) {
+
+        By username = By.name("username");
+        By password = By.name("password");
+        By login = By.name("login");
+        driver.findElement(By.xpath("//*[contains(text(),'Admin Login')]")).click();
+        driver.findElement(username).click();
+        driver.findElement(username).sendKeys(admin);
+        driver.findElement(password).click();
+        driver.findElement(password).sendKeys(adminPassword);
+
+        driver.findElement(login).click();
+//
+//        System.out.println("Login Done with Click");
+    }
+
+    public void clickSignInButton(String email,String password) throws InterruptedException {
+        driver.findElement(By.xpath("//*[contains(text(),'/ Sign In')]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).sendKeys(email);
+        driver.findElement(By.xpath("//input[@id='password'][@required='']")).click();
+        driver.findElement(By.xpath("//input[@id='password'][@required='']")).sendKeys(password);
+        driver.findElement(By.name("signin")).click();
+//        ExcelUtil.setExcelFileSheet("Login");
+    }
+
+    public void clickSignUp(XSSFRow row) throws InterruptedException {
+
+        driver.findElement(By.xpath("//*[contains(text(),'Sign Up')]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.name("fname")).click();
+        driver.findElement(By.name("fname")).sendKeys(row.getCell(1).toString());
+
+        driver.findElement(By.name("mobno")).click();
+        driver.findElement(By.name("mobno")).sendKeys(row.getCell(2).toString());
+
+        driver.findElement(By.name("email")).click();
+        driver.findElement(By.name("email")).sendKeys(row.getCell(3).toString());
+
+        driver.findElement(By.name("password")).click();
+        driver.findElement(By.name("password")).sendKeys(row.getCell(4).toString());
+
+        driver.findElement(By.name("repeatpassword")).click();
+        driver.findElement(By.name("repeatpassword")).sendKeys(row.getCell(5).toString());
+
+        driver.findElement(By.name("submit")).click();
+
+    }
+
+
+
+    public void signAgain(String email,String password) throws InterruptedException {
+        driver.findElement(By.xpath("//*[contains(text(),'/ Sign In')]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).sendKeys(email);
+        driver.findElement(By.xpath("//input[@id='password'][@required='']")).click();
+        driver.findElement(By.xpath("//input[@id='password'][@required='']")).sendKeys(password);
+        driver.findElement(By.name("signin")).click();
+//        ExcelUtil.setExcelFileSheet("Login");
+    }
+
+
+
 }
--- src/test/java/com/tms/user/userprofile/UserProfile.java
+++ src/test/java/com/tms/user/userprofile/UserProfile.java
@@ -1,4 +1,4 @@
-package com.tms.admin.userprofile;
+package com.tms.user.userprofile;
 
 import com.tms.base.BaseTest;
 import com.tms.pages.user.userprofile.userprofile;
@@ -27,34 +27,45 @@ public class UserProfile extends BaseTest {
 
 
     @Test
-    public void verifyThatAdminCanSuccessfullyLoginIntoAdminPanel() throws InterruptedException {
+    public void verifyThatUserCanSuccessfullyLoginIntoAdminPanel() throws InterruptedException {
         userprofileObj.clickSignInButton(ExcelUtil.getCellData(1,2),ExcelUtil.getCellData(1,1));
 //        commonStepsObj.saveTestResults(1,5);
 
     }
 
     @Test
-    public void verifyThatAdminNotAllowedLoginWithWrongUsernameAndCorrectPassword() throws InterruptedException {
+    public void verifyThatUserNotAllowedLoginWithWrongUsernameAndCorrectPassword() throws InterruptedException {
         userprofileObj.clickSignInButton(ExcelUtil.getCellData(2,2),ExcelUtil.getCellData(1,1));
 //        commonStepsObj.saveTestResults(1,5);
 
     }
 
     @Test
-    public void verifyThatAdminNotAllowedLoginWithCorrectUsernameAndWrongPassword() throws InterruptedException {
+    public void verifyThatUserNotAllowedLoginWithCorrectUsernameAndWrongPassword() throws InterruptedException {
         userprofileObj.clickSignInButton(ExcelUtil.getCellData(3,2),ExcelUtil.getCellData(1,1));
 //        commonStepsObj.saveTestResults(1,5);
 
     }
 
     @Test
-    public void verifyThatUAdminNotAllowedLoginWithEmptyUsernameAndEmptyPassword() throws InterruptedException {
+    public void verifyThatUserNotAllowedLoginWithEmptyUsernameAndEmptyPassword() throws InterruptedException {
         userprofileObj.clickSignInButton(ExcelUtil.getCellData(4,2),ExcelUtil.getCellData(1,1));
 //        commonStepsObj.saveTestResults(1,5);
 
     }
 
 
+    @Test
+    public void verifyThatNewUserCanSignUp() throws InterruptedException {
+        userprofileObj.clickSignUp(ExcelUtil.getRowData(8));
+
+    }
+
+    @Test
+    public void verifyThatNewUserCanSignUpWithAlreadyGivenUserDetails() throws InterruptedException {
+        userprofileObj.clickSignUp(ExcelUtil.getRowData(8));
+        userprofileObj.signAgain(ExcelUtil.getCellData(8,3),ExcelUtil.getCellData(8,4));
+    }
 
 }
 
